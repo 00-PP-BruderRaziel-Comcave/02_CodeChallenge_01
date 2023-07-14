9dec4905f5f5103f5a60ab83723500c4d2f281a2
BruderRaziel <nico.aldag@web.de>
2023-07-14 07:56:46 +0200

    ERste Iteration, Variablen sind strings

--- challenge_01.js
+++ challenge_01.js
@@ -9,3 +9,9 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+let zahl1 = prompt("Bitte die erste Zahl eingeben: ");
+let zahl2 = prompt("Bitte die zweite Zahl eingeben: ");
+
+let summe = zahl1 + zahl2;
+
+console.log("Summe ist: " + summe);
